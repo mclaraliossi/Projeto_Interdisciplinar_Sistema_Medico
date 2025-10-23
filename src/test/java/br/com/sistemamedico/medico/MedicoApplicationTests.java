130a6d5cecf57e6a9dfa16d4f7aae14474fde2bd
Maria Clara Liossi <mariaclaraliossi@gmail.com>
2025-10-23 17:17:23 -0300

    Inicializando o projeto 23/10

---
+++ src/test/java/br/com/sistemamedico/medico/MedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemamedico.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
