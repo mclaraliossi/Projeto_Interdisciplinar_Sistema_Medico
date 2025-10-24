e06a312d40577f4d893cd99ba93f7c0a1371cc6d
Nawanny <nawannysoares@gmail.com>
2025-10-24 16:09:15 -0300

    24/10

--- src/main/java/br/com/sistemamedico/medico/repository/ConsultaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sistemamedico.medico.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.sistemamedico.medico.entity.Consulta;
-
-public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
-    
-}
