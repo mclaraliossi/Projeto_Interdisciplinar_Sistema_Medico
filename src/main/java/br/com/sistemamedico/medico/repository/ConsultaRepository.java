42ef5db3d563b06ba4c27b5977e9186a85d459ff
Maria Clara Liossi <mariaclaraliossi@gmail.com>
2025-11-28 16:11:19 -0300

    Criação do dto, falta a repository

--- src/main/java/br/com/sistemamedico/medico/repository/ConsultaRepository.java
+++ src/main/java/br/com/sistemamedico/medico/repository/ConsultaRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.sistemamedico.medico.entity.Consulta;
 
@@ -14,5 +15,7 @@ public interface ConsultaRepository extends JpaRepository<Consulta, Integer> {
         boolean existsByClienteIdClienteAndDataehoraConsulta(Integer idCliente, LocalDateTime dataehoraConsulta);
 
         List<Consulta> findByMedicoIdMedico(Integer idMedico);
+
+        
 
 }
